21e6ce77f7374b809c672745ddc1db921bf4e612
sonenagisa <csna20061@g.nihon-u.ac.jp>
2024-06-28 02:22:36 +0000

    change main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include "mbed.h"
+#include "HEPTA_CDH.h"
+#include "HEPTA_EPS.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_CDH cdh(p5, p6, p7, p8, "sd");
+HEPTA_EPS eps(p16,p26);
+Timer sattime;
+ 
+int main(){
+    sattime.start();
+    float bt;
+    char str[100];
+    mkdir("/sd/mydir", 0777);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+    FILE *fp = fopen("/sd/mydir/test.txt","w");
+    if(fp == NULL) {error("Could not open file for write\r\n");}
+    pc.printf("Hello world!\r\n");
+    for(int i = 0; i < 10; i++) {
+        eps.vol(&bt);
+        fprintf(fp, "Time = %.2f [s] Vol = %.2f [V]\r\n", sattime.read(),bt);
+        wait(0.1);  
+    }
+    fclose(fp);
+
+    fp = fopen("/sd/mydir/test.txt","r");
+    for(int j = 0; j < 10; j++) {
+        fgets(str,100,fp);
+        pc.puts(str);
+    }
+    fclose(fp);
+    pc.printf("Goodbye!!\r\n");
 }
